8c299679567d1481a6e513b2a7022e8ba276c6da
Yoshi3lines <ny3line@gmail.com>
2020-11-01 10:39:15 +0000

    users-edit-add_finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
 
   private
     
@@ -61,5 +68,10 @@ class UsersController < ApplicationController
     # 現在ログインしているユーザーがアクセスしてきたユーザーと一致しているのか？
     def correct_user
       redirect_to(root_url) unless current_user?(@user) # current_user?はセッションヘルパーに定義
+    end
+    
+    # システム管理者なのかを判定する
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
