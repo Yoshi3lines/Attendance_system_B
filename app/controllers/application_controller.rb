05c25bbb18e6c0593d7fdee7722b2886d6ec09d3
Yoshi3lines <ny3line@gmail.com>
2020-11-15 03:25:12 +0000

    attendance-controller追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  # ページ出力前に１か月分のデータを確認、セットします。
+  def set_one_month
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date #三項演算子
+    @last_day = @first_day.end_of_month
+    one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
+    # ユーザーに紐づく１か月分のレコードを検索し取得します。
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+    
+  unless one_month.count == @attendances.count # 月の出勤日数と月の日数が一致するかを評価する。
+    ActiveRecord::Base.transaction do
+      one_month.each { |day| @user.attendances.create!(worked_on: day) }
+    end
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+  end
+  
+  rescue ActiveRecord::RecordInvalid #　トランザクションによるエラーの分岐処理
+    flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
+    redirect_to root_url
+  end
 end
