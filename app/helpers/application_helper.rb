e9fd5f8d246ab4381784928154dd55118ce8b6c4
Yoshi3lines <ny3line@gmail.com>
2020-11-25 11:02:27 +0000

    last-add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,23 @@
 module ApplicationHelper
     
-    # ページごとにタイトルを返す
-    def full_title(page_name = "")
-      base_title = "AttendanceApp"
-      if page_name.empty?
-          base_title
-      else
-          page_name + " | " + base_title
-      end
+  # ページごとにタイトルを返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+        base_title
+    else
+        page_name + " | " + base_title
     end
+  end
+  
+  def week_color(date)
+    displaying_label = %w(日 月 火 水 木 金 土)[DateTime.now.wday]
+    if DateTime.now.wday == "土"
+      tag.span(displaying_label, class: 'saturday')
+    elsif DateTime.now.wday == "日"
+      tag.span(displaying_label, class: 'sunday')
+    else
+      displaying_label
+    end
+  end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,10 +8,18 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤時間と退勤時間が存在する時に出勤時間より、退勤時間が早い場合は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # 出社もしくは退社のみの入力を無効にする
+  # validate :started_at_and_finished_at_present
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
+  
+  # def started_at_and_finished_at_present
+  #   errors.add("出社時間と退社時間は同時に入力しないといけません。")
+  #   if started_at.present? && finished_at.blank?
+  #   end
+  # end
   
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
