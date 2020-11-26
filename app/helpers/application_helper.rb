770736e48b4727ab9e86ffd0a0bd3a837c61980f
Yoshi3lines <ny3line@gmail.com>
2020-11-26 21:24:35 +0000

    round_gem_install

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,15 +9,4 @@ module ApplicationHelper
         page_name + " | " + base_title
     end
   end
-  
-  def week_color(date)
-    displaying_label = %w(日 月 火 水 木 金 土)[DateTime.now.wday]
-    if DateTime.now.wday == "土"
-      tag.span(displaying_label, class: 'saturday')
-    elsif DateTime.now.wday == "日"
-      tag.span(displaying_label, class: 'sunday')
-    else
-      displaying_label
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20201111104752) do
     t.string "remember_digest"
     t.boolean "admin"
     t.string "department"
-    t.datetime "basic_time", default: "2020-11-23 22:30:00"
-    t.datetime "work_time", default: "2020-11-23 23:00:00"
+    t.datetime "basic_time", default: "2020-11-26 22:30:00"
+    t.datetime "work_time", default: "2020-11-26 23:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
